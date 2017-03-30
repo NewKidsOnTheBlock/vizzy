a4e8db904b16e65d056742aa97dcc039f8323104
Corey Robinson <c7o7r7e7y@gmail.com>
2017-03-29 23:47:18 -0400

    Saving is implemented, there is no deletion yet lol.

--- app/app.js
+++ app/app.js
@@ -1,26 +1,43 @@
 (function () {'use strict';
 
 var os = require('os');
-var electron = require('electron');
 var fs = require('fs');
 
 // Here is the starting point for your application code.
 // All stuff below is just to show you how it works. You can delete all of it.
 
 // Use new ES6 modules syntax for everything.
+var remote = require('electron').remote; // native electron module
+var electronApp = require('electron').remote.app;
+var jetpack = require('fs-jetpack');
 var d3 = require('d3');
 
 const Vue = require('vue/dist/vue.common.js');
 var musicBar = require('../src/components/musicbar').musicBar;
 
 var Canvas = require('../src/components/canvas.js').Canvas;
+var Shape = require('../src/components/shape.js').Shape;
 var musicData = require('../src/components/musicdata.js').musicData;
+
+var SLASH = (function() {
+        if(process.platform === 'darwin' || process.platform === 'linux') {
+            return '/';
+        }
+        else return '\\'
+})();
+
+var VIZZY_PATH = electronApp.getPath('userData') + SLASH + 'vizzies';
 
 const app = new Vue({
     el: ".app",
     data: {
-        vizzies: [0,0,0,0,0,0,0,0],
-        canvas: new Canvas(),
+        directoryCheck: jetpack.exists(VIZZY_PATH),
+        vizzyDirectory: '',
+        vizzies: [],
+        vizzy: {
+            id: "Placeholder",
+            canvas: new Canvas(),
+        },
         musicInit: false,
         state: {
             home: true,
@@ -45,14 +62,18 @@ const app = new Vue({
         musicInitialize: function() {
             this.musicInit = true;
         },
-        moveState: function(page) {
+        moveState: function(page, index) {
             if(this.state.editor || this.state.player) {
                 //If we are leaving our editor/player state clear our svgs
-                this.canvas.clearShapeSvg();
+                this.vizzy.canvas.clearShapeSvg();
             }
+
+            if(this.state.editor) {
+                this.saveVizzy();
+            }
+
             for (var property in this.state) {
                 if (this.state.hasOwnProperty(property)) {
-                    console.log(property);
                     if(property === page) {
                         this.state[property] = true;
                     }
@@ -64,21 +85,72 @@ const app = new Vue({
             var app = this;
             window.setTimeout(function() {
                 if(page === 'editor' || page === 'player') {
-                    var svg = d3.select('svg');
-                    var svgCanvas = document.getElementById('svg');
-                    app.canvas.resize(svgCanvas.clientWidth, svgCanvas.clientHeight);
-                    app.canvas.setDomCanvas(svg);
+                    if(index === 0 || index) {
+                        console.log('setting saved vizzy');
+                        app.setVizzy(index);
+                    }
+                    else {
+                        console.log('new vizzy');
+                        var svg = d3.select('svg');
+                        var svgCanvas = document.getElementById('svg');
+                        app.vizzy.canvas.resize(svgCanvas.clientWidth, svgCanvas.clientHeight);
+                        app.vizzy.canvas.setDomCanvas(svg);
+                    }
                 }
             },0);
 
         },
-        selectShape: function(index) {
-            this.selectedShape.shape = this.canvas.shapes[index];
-            this.setColor();
-            for (var i = 0; i < this.canvas.shapes.length; i++) {
-                this.canvas.shapes[i].isSelected = false;
+        newVizzy: function() {
+            this.vizzy.id = 'Placeholder' + this.vizzies.length;
+        },
+        saveVizzy: function() {
+            jetpack.remove(VIZZY_PATH + SLASH + this.vizzy.id + '.json');
+            console.log(this.vizzy);
+            jetpack.write(VIZZY_PATH + SLASH + this.vizzy.id + '.json', this.vizzy);
+            this.updateVizzyList();
+            console.log('vizzy saved');
+        },
+        setVizzy: function(i) {
+            this.vizzy.canvas = new Canvas();
+            this.vizzy.id = this.vizzies[i].id;
+            console.log(this.vizzies[i]);
+
+            var svg = d3.select('svg');
+            var svgCanvas = document.getElementById('svg');
+            app.vizzy.canvas.resize(svgCanvas.clientWidth, svgCanvas.clientHeight);
+            app.vizzy.canvas.setDomCanvas(svg);
+
+            for(var j = 0; j < this.vizzies[i].canvas.shapes.length; j++) {
+                this.vizzy.canvas.add();
+                for (var property in this.vizzies[i].canvas.shapes[j]) {
+                    if (this.vizzies[i].canvas.shapes[j].hasOwnProperty(property)) {
+                        this.vizzy.canvas.shapes[j][property] = this.vizzies[i].canvas.shapes[j][property];
+                        console.log(property);
+                    }
+                }
             }
-            this.canvas.shapes[index].isSelected = true;
+            console.log(this.vizzy.canvas.shapes);
+        },
+        updateVizzyList: function() {
+            var foundVizzies = jetpack.list(VIZZY_PATH);
+            var parsedVizzies = [];
+            if (foundVizzies.length === 0) {
+                console.log('no vizzies found');
+            }
+            else {
+                for(var i = 0; i < foundVizzies.length; i++) {
+                    parsedVizzies.push(JSON.parse(jetpack.read(VIZZY_PATH + SLASH + foundVizzies[i])));
+                }
+            }
+            this.vizzies = parsedVizzies;
+        },
+        selectShape: function(index) {
+            this.selectedShape.shape = this.vizzy.canvas.shapes[index];
+            this.setColor();
+            for (var i = 0; i < this.vizzy.canvas.shapes.length; i++) {
+                this.vizzy.canvas.shapes[i].isSelected = false;
+            }
+            this.vizzy.canvas.shapes[index].isSelected = true;
         },
         toggleShapePanel: function(type) {
 
@@ -90,7 +162,7 @@ const app = new Vue({
             }
         },
         addShape: function() {
-            this.canvas.add();
+            this.vizzy.canvas.add();
         },
         setColor: function() {
             var minRed = this.selectedShape.shape.minColor.red;
@@ -109,6 +181,13 @@ const app = new Vue({
         }
     },
     mounted: function() {
+        if (this.directoryCheck) {
+            this.updateVizzyList();
+        }
+        else {
+            jetpack.dir(VIZZY_PATH);
+            this.vizzyDirectory = VIZZY_PATH;
+        }
 
         //initialize audio context & audio nodes
         var audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -129,7 +208,7 @@ const app = new Vue({
             //Only runs update if we are in the editor or play mode
             if(app.state.editor || app.state.player) {
                 mdata.update().then((ret) => {
-                    app.canvas.update(ret);
+                    app.vizzy.canvas.update(ret);
                 });
             }
             window.requestAnimationFrame(refresh);
--- src/app.js
+++ src/app.js
@@ -3,21 +3,38 @@
 
 // Use new ES6 modules syntax for everything.
 import os from 'os'; // native node.js module
-import { remote } from 'electron'; // native electron module
+var remote = require('electron').remote; // native electron module
+var electronApp = require('electron').remote.app
 import fs from 'fs';
+var jetpack = require('fs-jetpack');
 var d3 = require('d3');
 
 const Vue = require('vue/dist/vue.common.js');
 var musicBar = require('../src/components/musicbar').musicBar;
 
 var Canvas = require('../src/components/canvas.js').Canvas;
+var Shape = require('../src/components/shape.js').Shape;
 var musicData = require('../src/components/musicdata.js').musicData;
+
+var SLASH = (function() {
+        if(process.platform === 'darwin' || process.platform === 'linux') {
+            return '/';
+        }
+        else return '\\'
+})();
+
+var VIZZY_PATH = electronApp.getPath('userData') + SLASH + 'vizzies';
 
 const app = new Vue({
     el: ".app",
     data: {
-        vizzies: [0,0,0,0,0,0,0,0],
-        canvas: new Canvas(),
+        directoryCheck: jetpack.exists(VIZZY_PATH),
+        vizzyDirectory: '',
+        vizzies: [],
+        vizzy: {
+            id: "Placeholder",
+            canvas: new Canvas(),
+        },
         musicInit: false,
         state: {
             home: true,
@@ -42,14 +59,18 @@ const app = new Vue({
         musicInitialize: function() {
             this.musicInit = true;
         },
-        moveState: function(page) {
+        moveState: function(page, index) {
             if(this.state.editor || this.state.player) {
                 //If we are leaving our editor/player state clear our svgs
-                this.canvas.clearShapeSvg();
+                this.vizzy.canvas.clearShapeSvg();
             }
+
+            if(this.state.editor) {
+                this.saveVizzy();
+            }
+
             for (var property in this.state) {
                 if (this.state.hasOwnProperty(property)) {
-                    console.log(property);
                     if(property === page) {
                         this.state[property] = true;
                     }
@@ -61,21 +82,72 @@ const app = new Vue({
             var app = this;
             window.setTimeout(function() {
                 if(page === 'editor' || page === 'player') {
-                    var svg = d3.select('svg');
-                    var svgCanvas = document.getElementById('svg');
-                    app.canvas.resize(svgCanvas.clientWidth, svgCanvas.clientHeight);
-                    app.canvas.setDomCanvas(svg);
+                    if(index === 0 || index) {
+                        console.log('setting saved vizzy');
+                        app.setVizzy(index);
+                    }
+                    else {
+                        console.log('new vizzy');
+                        var svg = d3.select('svg');
+                        var svgCanvas = document.getElementById('svg');
+                        app.vizzy.canvas.resize(svgCanvas.clientWidth, svgCanvas.clientHeight);
+                        app.vizzy.canvas.setDomCanvas(svg);
+                    }
                 };
             },0);
 
         },
-        selectShape: function(index) {
-            this.selectedShape.shape = this.canvas.shapes[index];
-            this.setColor();
-            for (var i = 0; i < this.canvas.shapes.length; i++) {
-                this.canvas.shapes[i].isSelected = false;
+        newVizzy: function() {
+            this.vizzy.id = 'Placeholder' + this.vizzies.length;
+        },
+        saveVizzy: function() {
+            jetpack.remove(VIZZY_PATH + SLASH + this.vizzy.id + '.json');
+            console.log(this.vizzy);
+            jetpack.write(VIZZY_PATH + SLASH + this.vizzy.id + '.json', this.vizzy);
+            this.updateVizzyList();
+            console.log('vizzy saved');
+        },
+        setVizzy: function(i) {
+            this.vizzy.canvas = new Canvas();
+            this.vizzy.id = this.vizzies[i].id;
+            console.log(this.vizzies[i]);
+
+            var svg = d3.select('svg');
+            var svgCanvas = document.getElementById('svg');
+            app.vizzy.canvas.resize(svgCanvas.clientWidth, svgCanvas.clientHeight);
+            app.vizzy.canvas.setDomCanvas(svg);
+
+            for(var j = 0; j < this.vizzies[i].canvas.shapes.length; j++) {
+                this.vizzy.canvas.add();
+                for (var property in this.vizzies[i].canvas.shapes[j]) {
+                    if (this.vizzies[i].canvas.shapes[j].hasOwnProperty(property)) {
+                        this.vizzy.canvas.shapes[j][property] = this.vizzies[i].canvas.shapes[j][property];
+                        console.log(property);
+                    }
+                }
             }
-            this.canvas.shapes[index].isSelected = true;
+            console.log(this.vizzy.canvas.shapes);
+        },
+        updateVizzyList: function() {
+            var foundVizzies = jetpack.list(VIZZY_PATH);
+            var parsedVizzies = [];
+            if (foundVizzies.length === 0) {
+                console.log('no vizzies found');
+            }
+            else {
+                for(var i = 0; i < foundVizzies.length; i++) {
+                    parsedVizzies.push(JSON.parse(jetpack.read(VIZZY_PATH + SLASH + foundVizzies[i])));
+                }
+            }
+            this.vizzies = parsedVizzies;
+        },
+        selectShape: function(index) {
+            this.selectedShape.shape = this.vizzy.canvas.shapes[index];
+            this.setColor();
+            for (var i = 0; i < this.vizzy.canvas.shapes.length; i++) {
+                this.vizzy.canvas.shapes[i].isSelected = false;
+            }
+            this.vizzy.canvas.shapes[index].isSelected = true;
         },
         toggleShapePanel: function(type) {
 
@@ -87,7 +159,7 @@ const app = new Vue({
             }
         },
         addShape: function() {
-            this.canvas.add();
+            this.vizzy.canvas.add();
         },
         setColor: function() {
             var minRed = this.selectedShape.shape.minColor.red;
@@ -106,6 +178,13 @@ const app = new Vue({
         }
     },
     mounted: function() {
+        if (this.directoryCheck) {
+            this.updateVizzyList();
+        }
+        else {
+            jetpack.dir(VIZZY_PATH);
+            this.vizzyDirectory = VIZZY_PATH;
+        }
 
         //initialize audio context & audio nodes
         var audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -126,7 +205,7 @@ const app = new Vue({
             //Only runs update if we are in the editor or play mode
             if(app.state.editor || app.state.player) {
                 mdata.update().then((ret) => {
-                    app.canvas.update(ret);
+                    app.vizzy.canvas.update(ret);
                 });
             }
             window.requestAnimationFrame(refresh);
